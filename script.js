36372092c6016c2420472de00a8c2e880555689a
ZKatok <zkatok@sageridge.org>
2021-04-03 14:58:03 -0500

    test canvas

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var canvasWidth = 1000;
+var canvasHeight = 600;
+
+function setup() {
+  var myCanvas = createCanvas(canvasWidth, canvasHeight);
+  //myCanvas.parent("p5container");
+}
+
+function draw() {
+  background(220);
+}
