a3245d71c6a61757964561f4fe32ab48c4f0d4c7
meng-bop <81833807+meng-bop@users.noreply.github.com>
2021-04-04 10:26:23 -0500

    Create script.js

--- script.js
+++ script.js
@@ -7,20 +7,19 @@ let numCols = canvasHeight/rectSide;
 let pixelArr = [];
 let t = 0;
 let colorArr =
-[[color(345, 100, 60), color(345, 100, 70), color(345, 100, 74), color(345, 100, 81) color(345, 100, 88)color(100, 100, 100, 0)], // reds
-[color(117, 100, 53), color(117, 100, 62), color(117, 100, 69), color(117, 100, 78) color(117, 100, 85) color(100, 100, 100, 0)], // greens
-[color(227, 100, 56), color(227, 100, 62), color(227, 100, 67), color(227, 100, 73) color(227, 100, 79) color(100, 100, 100, 0)], // purples
-[color(284, 100, 50), color(284, 100, 58), color(284, 100, 64), color(284, 100, 73) color(284, 100, 81) color(100, 100, 100, 0)],//blues
-[color(71, 100, 64), color(71, 100, 70), color(71, 100, 74), color(71, 100, 79) color(71, 100, 86) color(100, 100, 100, 0)],//yellows
-];
-let colorSet = 0;
+[[345, 100, 60], [345, 100, 70], [345, 100, 74], [345, 100, 81], [345, 100, 88],[100, 100, 100, 0]], // reds
+[[117, 100, 53], [117, 100, 62], [117, 100, 69], [117, 100, 78], [117, 100, 85], [100, 100, 100, 0]], // greens
+[[227, 100, 56], [227, 100, 62], [227, 100, 67], [227, 100, 73], [227, 100, 79], [100, 100, 100, 0]], // purples
+[[284, 100, 50], [284, 100, 58], [284, 100, 64], [284, 100, 73] ,[284, 100, 81],[100, 100, 100, 0]],//blues
+[[71, 100, 64], [71, 100, 70], [71, 100, 74], [71, 100, 79], [71, 100, 86], [100, 100, 100, 0]],//yellows
+ ]
 function setup() {
   let myCanvas = createCanvas(canvasWidth, canvasHeight);
   rectMode(CORNER);
   noStroke();
   noiseDetail(4, 0.6);
   frameRate(1);
-  colorMode(HSL, 360, 100, 100, 100);
+  colorMode(HSL, 360, 100, 100);
 
   background(220);
 
@@ -46,17 +45,17 @@ function draw() {
 
 function transformNoise(n) {
   if (n < 0.5) {
-    return 5; // sky
-  } else if (n < 0.6) {
     return 0;
+  } else if (n < 0.6) {
+    return 0.2;
   } else if (n < 0.7) {
-    return 1;
+    return 0.4;
   } else if (n < 0.8) {
-    return 2;
+    return 0.5;
   } else if (n < 0.9) {
-    return 3;
+    return 0.8;
   } else {
-    return 4;
+    return 1;
   }
 }
 
@@ -67,7 +66,7 @@ function changePixel(xpos, ypos, value) {
 function drawPixelArr() {
   for (let r = 0; r < numRows; r++) {
     for (let c = 0; c < numCols; c++) {
-      fill(colorArr[colorSet][pixelArr[r*numCols + c]][0], colorArr[colorSet][pixelArr[r*numCols + c]][1], colorArr[colorSet][pixelArr[r*numCols + c]][2]);
+      fill(color(pixelArr[r*numCols + c]));
       // strokeWeight(1); //uncomment to see pixel outlines
       // stroke('red');
       rect(r*rectSide, c*rectSide, rectSide, rectSide);
